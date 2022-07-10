4773c328617f446653ad22a6fe826192a3fbc5c6
Honza Semerák <deinorkh@gmail.com>
2022-07-11 00:39:19 +0200

    Updated and improved gui. Added value of used hours previous month.

--- excel_script.py
+++ excel_script.py
@@ -1,38 +1,58 @@
-import os 
-import openpyxl
-from openpyxl.utils.cell import coordinate_from_string, column_index_from_string    #Import for geting row number
-
 #Creating dictionary with keys and empty values. Used values needs to be epty to create first instance of table. Will be replaced by data from project after generate_data) 
-used_data = {"project_number": "",
+used_dataFC = {"project_number": "",
             "s_nr": " ",
             "pjm_de": " ",
             "pjm_de": " ",
             "ordered": " ",
             "used_hours": " ",
-            "available": " "}
+            "available": " ",
+            "used_lastm_hours": " "}
 
-def import_os():
-    print(os.listdir("."))
+def get_dataFC (row_FC, sheet_FC, previous_month):  #Get row_FC number
+    
+    if previous_month == "January": #Used month in last month
+        used_dataFC["used_lastm_hours"] = sheet_FC["L" + str(row_FC)].value 
+    elif previous_month == "February":
+        used_dataFC["used_lastm_hours"] = sheet_FC["M" + str(row_FC)].value  
+    elif previous_month == "March":
+        used_dataFC["used_lastm_hours"] = sheet_FC["N" + str(row_FC)].value 
+    elif previous_month == "April":
+        used_dataFC["used_lastm_hours"] = sheet_FC["O" + str(row_FC)].value 
+    elif previous_month == "May":
+        used_dataFC["used_lastm_hours"] = sheet_FC["P" + str(row_FC)].value 
+    elif previous_month == "June":
+        used_dataFC["used_lastm_hours"] = sheet_FC["Q" + str(row_FC)].value 
+    elif previous_month == "July":
+        used_dataFC["used_lastm_hours"] = sheet_FC["R" + str(row_FC)].value 
+    elif previous_month == "August":
+        used_dataFC["used_lastm_hours"] = sheet_FC["S" + str(row_FC)].value 
+    elif previous_month == "September":
+        used_dataFC["used_lastm_hours"] = sheet_FC["T" + str(row_FC)].value 
+    elif previous_month == "October":
+        used_dataFC["used_lastm_hours"] = sheet_FC["U" + str(row_FC)].value 
+    elif previous_month == "November":
+        used_dataFC["used_lastm_hours"] = sheet_FC["V" + str(row_FC)].value 
+    elif previous_month == "December":
+        used_dataFC["used_lastm_hours"] = sheet_FC["W" + str(row_FC)].value 
 
-def get_data (row, sheet):  #Get row number
+    used_dataFC["project_number"] = sheet_FC ["A"+str(row_FC)].value  #Get project name
+    used_dataFC["s_nr"] = sheet_FC ["C" + str(row_FC)].value #Get S-Number
+    used_dataFC["pjm_de"] = sheet_FC["D"+ str(row_FC)].value
+    used_dataFC["ordered"] = sheet_FC["F"+ str(row_FC)].value  #Get ordered hours
+    used_dataFC["used_hours"] = sheet_FC["G"+ str(row_FC)].value  #Get used hours
+    used_dataFC["available"] = sheet_FC["H" + str(row_FC)].value  #Get available hours
     
     
-    used_data["project_number"] = sheet ["A"+str(row)].value  #Get project name
-    used_data["s_nr"] = sheet ["C" + str(row)].value #Get S-Number
-    used_data["pjm_de"] = sheet["D"+ str(row)].value
-    used_data["ordered"] = sheet["F"+ str(row)].value  #Get ordered hours
-    used_data["used_hours"] = sheet["G"+ str(row)].value  #Get used hours
-    used_data["available"] = sheet["H" + str(row)].value  #Get available hours
-    return used_data
-    #print("Excel script", used_data)    #Debug_1_3
+    return used_dataFC
+    #print("Excel script", used_dataFC)    #Debug_1_3
 
 #import_os()
 #test = get_listssnr()
 #print(get_listssnr())
 #test3 = get_row()
-#test4 = get_data()
+#test4 = get_dataFC()
 
 
-#print(row, "Global")
+#print(row_FC, "Global")
 #print(list_snr, "Global")   #Debug_2_1
-#print (used_data, "global")    #Debug_2_2
+#print (used_dataFC, "global")    #Debug_2_2
--- gui.py
+++ gui.py
@@ -2,24 +2,40 @@ from operator import index
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 import openpyxl
+import datetime
+import calendar
 from openpyxl.utils.cell import coordinate_from_string
-from excel_script import get_data, used_data
+from excel_script import get_dataFC, used_dataFC
 
-wb = openpyxl.load_workbook('final-confirmed_2022_6_23.xlsx')
-wb.sheetnames           # The workbook's sheets' names.
-sheet = wb ["CAD"]      #Get a sheet from workbook  
-#print(sheet)            #Debug_1
-    
+#Load curent date, curent month andd previous month
+current_date = datetime.datetime.now()      #Curent date.time type of return
+curent_month = current_date.strftime("%B")  #Curent month date.time type of return 
+previous_month = calendar.month_name[datetime.datetime.now().month - 1] #Previous month
+if previous_month == 0:
+    previous_month =12
+
+#Load data from final-confirmed excel
+wb_FC = openpyxl.load_workbook('final-confirmed_2022_6_23.xlsx')
+wb_FC.sheetnames           # The workbook's sheets' names.
+sheet_FC = wb_FC ["CAD"]      #Get a sheet from workbook  
+#print(sheet_FC)            #Debug_1
+
+#Load data from yellow-blue excel
+#wb_YB = openpyxl.load_workbook('yellow-blue_2022_6_23.xlsx')
+#wb_YB.sheetnames           # The workbook's sheets' names.
+#sheet_YB = wb_YB ["CAD"]      #Get a sheet from workbook  
+#print(sheet_YB)            #Debug_1
+ 
 # Get list of projects
-s_nr_col = sheet ["C"]
+s_nr_col = sheet_FC ["C"]
 list_snr = []
 for x in range(4, len(s_nr_col)):       #S-number list
     list_snr += [s_nr_col[x].value]
 
 #print(list_snr)                         #Debug_1_1
 
-#Load empty used data dictionary
-dic = used_data
+#Load empty used_data dictionary
+dic_FC = used_dataFC
     
 class MainWindow(qtw.QWidget):
     def __init__(self):
@@ -28,11 +44,67 @@ class MainWindow(qtw.QWidget):
         #Add Title
         self.setWindowTitle("Johny XXX")
 
+        #Midnight Pebble Beach
+        color_window =  """
+            QWidget {
+                background-color: rgb(237,245,244);
+                }
+            """
+        color_button = """
+            QWidget {
+                border-radius: 8px;
+                background-color: rgb(46,92,87);
+                font-size: 18px;
+                padding: 10px;
+                font-weight: 500;
+                color: white
+                }
+            QPushButton:hover {
+                border: 4px solid rgb(141,205,198);
+                border-radius: 8px;
+                background-color: rgb(93,184,174);
+                font-size: 18px;
+                color: black               
+                }
+            """
+        color_table = """
+            QWidget {
+                background-color: rgb(56,112,106);
+                color: white;
+                font-weight: 500;
+                }
+            QHeaderView:section{
+                Background-color:rgb(7,17,37);
+                font-weight: 500;
+                border-radius: 15px;
+            }
+        """
+        color_label = """
+            QWidget {
+                background-color: rgb(133,189,182);
+                font-size: 22px;
+                color: black;
+                border-radius: 15px;
+                padding: 5px;
+                font-weight: 500;
+                }
+        """
+        color_combobox = """
+              QWidget {
+                background-color: rgb(133,189,182);
+                padding: 5px;
+                padding-left: 20px;
+                color: black;
+                border-radius: 15px;
+              }  
+        """
+
         #Creating layouts
         self.setGeometry(600, 300, 800, 600)
         outer_layout = qtw.QVBoxLayout()
         top_layout = qtw.QFormLayout()
         body_layout = qtw.QVBoxLayout()
+        self.setStyleSheet(color_window)
 
         #Nesting layouts
         outer_layout.addLayout(top_layout)
@@ -43,9 +115,11 @@ class MainWindow(qtw.QWidget):
 
         #Create lable for list SNR
         label_snr = qtw.QLabel("Select S-Number:")
+        label_snr.setStyleSheet(color_label)
        
         #Change font, text size of lable
         label_snr.setFont(qtg.QFont("Helvetica", 15))
+
         top_layout.addWidget(label_snr)
 
         #Create Combo box
@@ -53,6 +127,7 @@ class MainWindow(qtw.QWidget):
 
         #Add Items to combo Box
         my_combo.addItems(list_snr)
+        my_combo.setStyleSheet(color_combobox)
 
         #Change font, text size of lable
         my_combo.setFont(qtg.QFont("Helvetica", 10))
@@ -71,23 +146,29 @@ class MainWindow(qtw.QWidget):
         #Create table 
         table1 = qtw.QTableWidget(self)
         table1.setRowCount(1)
-        table1.setColumnCount(6)
-        table1.setHorizontalHeaderLabels(["Project number", "S-Number", "Project leader DE", "Ordered hours", "Used hours", "Available hours"])
-        table1.setVerticalHeaderLabels(["", "", "", "", "", ""])
+        table1.setColumnCount(7)
+        table1.setHorizontalHeaderLabels(["Project number", "S-Number", "Project leader DE", "Ordered hours", "Used hours", "Available hours", "Used hours in last month"])
+        table1.verticalHeader().setVisible(False)
+        table1.setStyleSheet(color_table)
+        table1.horizontalHeader().setStyleSheet(color_table)
 
         #Fill table with dictionary values andd keys 
-        table1.setItem(0, 0, qtw.QTableWidgetItem(str(dic["project_number"])))
-        table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic["s_nr"])))
-        table1.setItem(0, 2, qtw.QTableWidgetItem(str(dic["pjm_de"])))
-        table1.setItem(0, 3, qtw.QTableWidgetItem(str(dic["ordered"])))
-        table1.setItem(0, 4, qtw.QTableWidgetItem(str(dic["used_hours"])))
-        table1.setItem(0, 5, qtw.QTableWidgetItem(str(dic["available"])))
+        table1.setItem(0, 0, qtw.QTableWidgetItem(str(dic_FC["project_number"])))
+        table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic_FC["s_nr"])))
+        table1.setItem(0, 2, qtw.QTableWidgetItem(str(dic_FC["pjm_de"])))
+        table1.setItem(0, 3, qtw.QTableWidgetItem(str(dic_FC["ordered"])))
+        table1.setItem(0, 4, qtw.QTableWidgetItem(str(dic_FC["used_hours"])))
+        table1.setItem(0, 5, qtw.QTableWidgetItem(str(dic_FC["available"])))
+        table1.setItem(0, 6, qtw.QTableWidgetItem(str(dic_FC["used_lastm_hours"])))
+            
         
         #Put table on screen
         body_layout.addWidget(table1)
         
         #Create a button
         button_generateOut = qtw.QPushButton("Generate e-mail", clicked = lambda: generate_email())
+         
+        button_generateOut.setStyleSheet(color_button)
         body_layout.addWidget(button_generateOut)
 
         #Generate out. data
@@ -96,27 +177,29 @@ class MainWindow(qtw.QWidget):
             cell_address = "C" + str(list_snrIndex + 5)
             #print(cell_address) #Debug 2_2
 
-            #Get selected row number
+            #Get selected row_FC number
 
             xy = coordinate_from_string (cell_address)
-            row = xy [1]
-            #print(row)          #Debug 2_3
+            row_FC = xy [1]
+            #print(row_FC)          #Debug 2_3
         
-            dic = get_data(row, sheet)
-            #print(dic) #Debug 2_4
+            dic_FC = get_dataFC(row_FC, sheet_FC, curent_month)
+            #print(dic_FC) #Debug 2_4
 
             #Update table dictionary values each time project is selected in combo box
-            table1.setItem(0, 0, qtw.QTableWidgetItem(str(dic["project_number"])))
-            table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic["used_hours"])))
-            table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic["s_nr"])))
-            table1.setItem(0, 2, qtw.QTableWidgetItem(str(dic["pjm_de"])))
-            table1.setItem(0, 3, qtw.QTableWidgetItem(str(dic["ordered"])))
-            table1.setItem(0, 4, qtw.QTableWidgetItem(str(dic["used_hours"])))
-            table1.setItem(0, 5, qtw.QTableWidgetItem(str(dic["available"])))
+            table1.setItem(0, 0, qtw.QTableWidgetItem(str(dic_FC["project_number"])))
+            table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic_FC["used_hours"])))
+            table1.setItem(0, 1, qtw.QTableWidgetItem(str(dic_FC["s_nr"])))
+            table1.setItem(0, 2, qtw.QTableWidgetItem(str(dic_FC["pjm_de"])))
+            table1.setItem(0, 3, qtw.QTableWidgetItem(str(dic_FC["ordered"])))
+            table1.setItem(0, 4, qtw.QTableWidgetItem(str(dic_FC["used_hours"])))
+            table1.setItem(0, 5, qtw.QTableWidgetItem(str(dic_FC["available"])))
+            table1.setItem(0, 6, qtw.QTableWidgetItem(str(dic_FC["used_lastm_hours"])))
+            
 
          #Generate email
         def generate_email():
-            print("E-mail generated!")
+            print("E-mail generated!", used_dataFC)
 
         #Show the all
         self.show()
